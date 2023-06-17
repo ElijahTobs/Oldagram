7e3007a39e51f62119bd673779c6869bd88d5002
Comfort Ige <eligenius@gmail.com>
2023-06-17 14:25:14 +0100

    Add posts array

---
+++ index.js
@@ -0,0 +1,30 @@
+const posts = [
+  {
+    name: "Cristiano Ronaldo",
+    username: "elijahDev",
+    location: "Lagos, Nigeria",
+    avatar: "img/c-ron1.jpg",
+    post: "img/ronaldo1.jpg",
+    comment: "You're the greatest football player alive. The G.O.A.T",
+    likes: "14,382,195"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    username: "elijahDev",
+    location: "Lagos, Nigeria",
+    avatar: "img/c-ron1.jpg",
+    post: "img/ronaldo2.jpg",
+    comment: "You're the greatest football player alive. The G.O.A.T",
+    likes: "15,496,335"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    username: "elijahDEev",
+    location: "Lagos, Nigeria",
+    avatar: "img/c-ron1.jpg",
+    post: "img/ronaldo3.jpg",
+    comment: "You're the greatest football player alive. The G.O.A.T",
+    likes: "13,218,729"
+  },
+
+]
