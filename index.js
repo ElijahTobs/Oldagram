126eb7133599795cef7036a915440c6f55aee2f9
Comfort Ige <eligenius@gmail.com>
2023-06-17 14:45:19 +0100

    Add variables for each object item

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const posts = [
     name: "Cristiano Ronaldo",
     username: "elijahDev",
     location: "Lagos, Nigeria",
+    my_avatar: "img/elijah-1.jpg",
     avatar: "img/c-ron1.jpg",
     post: "img/ronaldo1.jpg",
     comment: "You're the greatest football player alive. The G.O.A.T",
@@ -12,6 +13,7 @@ const posts = [
     name: "Cristiano Ronaldo",
     username: "elijahDev",
     location: "Lagos, Nigeria",
+    my_avatar: "img/elijah-1.jpg",
     avatar: "img/c-ron1.jpg",
     post: "img/ronaldo2.jpg",
     comment: "You're the greatest football player alive. The G.O.A.T",
@@ -21,6 +23,7 @@ const posts = [
     name: "Cristiano Ronaldo",
     username: "elijahDEev",
     location: "Lagos, Nigeria",
+    my_avatar: "img/elijah-1.jpg",
     avatar: "img/c-ron1.jpg",
     post: "img/ronaldo3.jpg",
     comment: "You're the greatest football player alive. The G.O.A.T",
@@ -28,3 +31,22 @@ const posts = [
   },
 
 ]
+
+function renderCard() {
+  for (let x in posts){
+    const name = posts[x].name
+    const username = posts[x].username
+    const location = posts[x].location
+    const my_avatar = posts[x].my_avatar
+    const avatar = posts[x].avatar
+    const post = posts[x].post
+    const comment = posts[x].comment
+    const likes = posts[x].likes
+
+
+   
+
+  }
+
+
+}
